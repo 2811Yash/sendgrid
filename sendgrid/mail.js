427e0a90079c2ddf09607866e91445650f347ed4
Yash Santosh Patil <2811yashpatil@gmail.com>
2025-04-15 13:00:25 +0530

    /

--- sendgrid/mail.js
+++ sendgrid/mail.js
@@ -22,6 +22,7 @@ app.use(express.json());
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.post('/send-emails', async (req, res) => {
+  console.log("started backend request");
   req.res.send("hello");
   const emails = req.body.emails;
   if (!emails || !Array.isArray(emails)) {
