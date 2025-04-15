fcef7923ef25b6f4eff9b4f049f4eb42c117394b
2811Yash <118837106+2811Yash@users.noreply.github.com>
2025-04-15 14:58:26 +0530

    Update page.tsx

--- email_test_app/src/app/page.tsx
+++ email_test_app/src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://sendgrid-qyoy.vercel.app/send-emails', {
+      const response = await fetch('https://sendgrid-ax0i.onrender.com/send-emails', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ emails }),
